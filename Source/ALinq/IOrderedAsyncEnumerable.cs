396ee579e715dd99384e3f13bac582f75327837b
Bernhard Glueck <business@bernhardglueck.com>
2012-08-20 15:58:03 +0200

    Added ordered async enumerable interface for ThenBy functionality.

---
+++ Source/ALinq/IOrderedAsyncEnumerable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALinq
+{
+    public interface IOrderedAsyncEnumerable<out T> : IAsyncEnumerable<T>
+    {
+        IOrderedAsyncEnumerable<T> CreateOrderedEnumerable<TKey>(Func<T, Task<TKey>> keySelector, IComparer<TKey> comparer, bool descendind);
+    }
+}
