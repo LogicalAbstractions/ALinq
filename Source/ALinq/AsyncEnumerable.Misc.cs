878f0b3e17fc72b4504cfc570a120f19a9985639
Bernhard Glueck <business@bernhardglueck.com>
2012-08-20 14:52:12 +0200

    Added RX to ALINQ conversion

--- Source/ALinq/AsyncEnumerable.Misc.cs
+++ Source/ALinq/AsyncEnumerable.Misc.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ALinq
@@ -36,6 +37,67 @@ namespace ALinq
 #pragma warning disable 1998
             return new ConcurrentAsyncEnumerable<T>(async producer => {});
 #pragma warning restore 1998
+        }
+
+        public static IAsyncEnumerable<T> ToAsync<T>(this IEnumerable<T> enumerable)
+        {
+            if (enumerable == null) throw new ArgumentNullException("enumerable");
+
+            return AsyncEnumerable.Create<T>(async producer =>
+            {
+                foreach (var item in enumerable)
+                {
+                    await producer.Yield(item);
+                }
+            });
+        }
+
+        public static IAsyncEnumerable<T> ToAsync<T>(this IEnumerator<Task<T>> enumerator)
+        {
+            if (enumerator == null) throw new ArgumentNullException("enumerator");
+
+            var wasRetrieved = false;
+
+            return ToAsync(() =>
+            {
+                if (wasRetrieved)
+                {
+                    throw new InvalidOperationException("Sequence can only be evaluated once when created from raw enumerator");
+                }
+
+                wasRetrieved = true;
+                return enumerator;
+            });
+        }
+
+        public static IAsyncEnumerable<T> ToAsync<T>(this IEnumerable<Task<T>> enumerable)
+        {
+            if (enumerable == null) throw new ArgumentNullException("enumerable");
+
+            return ToAsync(enumerable.GetEnumerator);
+        }
+
+        public static IAsyncEnumerable<T> ToAsync<T>(Func<IEnumerator<Task<T>>> enumeratorFactory)
+        {
+            if (enumeratorFactory == null) throw new ArgumentNullException("enumeratorFactory");
+
+            return new AsyncEnumerableConverter<T>(enumeratorFactory);
+        }
+
+        public static IAsyncEnumerable<T> ToAsync<T>(this IObservable<T> observable)
+        {
+            if (observable == null) throw new ArgumentNullException("observable");
+
+            return AsyncEnumerable.Create<T>(async producer => {
+            
+                using( var converter = new ConversionObserver<T>(producer))
+                {
+                    using (observable.Subscribe(converter))
+                    {
+                        await converter.WaitForCompletion();
+                    }
+                }
+            });
         }
 
         internal static void Dispose(this IAsyncEnumerator asyncEnumerator)
---
+++ Source/ALinq/ConversionObserver.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ALinq
+{
+    internal sealed class ConversionObserver<T> : IObserver<T>,IDisposable 
+    {
+        private readonly ConcurrentAsyncProducer<T> producer;
+        private readonly SemaphoreSlim              completedSemaphore = new SemaphoreSlim(0, 1);
+        private Exception                           exception;
+
+        internal async Task WaitForCompletion()
+        {
+            await completedSemaphore.WaitAsync();
+
+            if (exception != null)
+            {
+                throw exception;
+            }
+        }
+
+        void IObserver<T>.OnCompleted()
+        {
+            try
+            {
+                completedSemaphore.Release();
+            }
+            catch(SemaphoreFullException)
+            {
+            
+            }
+        }
+
+        void IObserver<T>.OnError(Exception error)
+        {
+            try
+            {
+                exception = error;
+                completedSemaphore.Release();
+            }
+            catch (SemaphoreFullException)
+            {
+
+            }
+        }
+
+        void IObserver<T>.OnNext(T value)
+        {
+            producer.Yield(value).Wait();
+        }
+
+        void IDisposable.Dispose()
+        {
+            completedSemaphore.Dispose();
+        }
+
+        internal ConversionObserver(ConcurrentAsyncProducer<T> producer)
+        {
+            this.producer = producer;
+        }
+    }
+}
