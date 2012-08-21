9d7952e0ebea3e32fc01d17d22a3feb297d47ae1
Bernhard Glueck <business@bernhardglueck.com>
2012-08-21 14:24:42 +0200

    Added single operators.

--- Source/ALinq/AsyncEnumerable.Evaluation.cs
+++ Source/ALinq/AsyncEnumerable.Evaluation.cs
@@ -58,6 +58,86 @@ namespace ALinq
             throw new InvalidOperationException("Sequence contains no matching element");
         }
 
+        public static async Task<T> Single<T>(this IAsyncEnumerable<T> enumerable)
+        {
+#pragma warning disable 1998
+            return await First<T>(enumerable, async item => true);
+#pragma warning restore 1998
+        }
+
+        public static async Task<T> Single<T>(this IAsyncEnumerable<T> enumerable, Func<T, Task<bool>> predicate)
+        {
+            if (enumerable == null) throw new ArgumentNullException("enumerable");
+            if (predicate == null) throw new ArgumentNullException("predicate");
+
+            var result  = default(T);
+            var counter = 0;
+
+            await enumerable.ForEach(async state =>
+            {
+                if (await predicate(state.Item))
+                {
+                    counter++;
+                    result = state.Item;
+
+                    if (counter > 1)
+                    {
+                        state.Break();
+                    }
+                }
+            });
+
+            if (counter == 0)
+            {
+                throw new InvalidOperationException("Sequence contains no matching element");
+            }
+
+            if (counter == 1)
+            {
+                return result;
+            }
+
+            throw new InvalidOperationException("Sequence contains more than one element");
+        }
+
+        public static async Task<T> SingleOrDefault<T>(this IAsyncEnumerable<T> enumerable)
+        {
+#pragma warning disable 1998
+            return await First<T>(enumerable, async item => true);
+#pragma warning restore 1998
+        }
+
+        public static async Task<T> SingleOrDefault<T>(this IAsyncEnumerable<T> enumerable, Func<T, Task<bool>> predicate)
+        {
+            if (enumerable == null) throw new ArgumentNullException("enumerable");
+            if (predicate == null) throw new ArgumentNullException("predicate");
+
+            var result = default(T);
+            var counter = 0;
+
+            await enumerable.ForEach(async state =>
+            {
+                if (await predicate(state.Item))
+                {
+                    counter++;
+                    result = state.Item;
+
+                    if (counter > 1)
+                    {
+                        state.Break();
+                    }
+                }
+            });
+
+
+            if (counter == 1)
+            {
+                return result;
+            }
+
+            return default(T);
+        }
+
         public static async Task<T> FirstOrDefault<T>(this IAsyncEnumerable<T> enumerable)
         {
 #pragma warning disable 1998
