09d43e6c47f765f5e479c70c8f35183383d9a14b
BernhardGlueck <business@bernhardglueck.com>
2012-08-21 19:55:34 +0200

    Added group join

--- Readme.md
+++ Readme.md
@@ -1,11 +1,12 @@
 # ALINQ - Asynchronous LINQ to Objects query processing
 
 Currently we are at a very limited alpha release state, a lot of things are still
-being worked on and in flux, and quite a few operators are not implemented yet.
+being worked on and in flux.
+By now most standard LINQ operators are implemented, but test coverage is still low, and is the next
+focus in development. 
 
-Missing LINQ operators:
+A few operators are still missing as listed below:
 
- - GroupJoin
  - ThenBy
  - ThenByDescending
  - ToLookup
---
+++ Source/ALinq/AsyncEnumerable.GroupJoin.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ALinq
+{
+    public static partial class AsyncEnumerable
+    {
+        public static IAsyncEnumerable<TResult> GroupJoin<TOuter, TInner, TKey, TResult>(this IAsyncEnumerable<TOuter> outer,IAsyncEnumerable<TInner> inner,
+                                                                                    Func<TOuter, Task<TKey>> outerKeySelector,
+                                                                                    Func<TInner, Task<TKey>> innerKeySelector,
+                                                                                    Func<TOuter,IAsyncEnumerable<TInner>, Task<TResult>> resultSelector)
+        {
+            return GroupJoin(outer, inner, outerKeySelector, innerKeySelector, resultSelector, EqualityComparer<TKey>.Default);
+        }
+
+        public static IAsyncEnumerable<TResult> GroupJoin<TOuter, TInner, TKey, TResult>(this IAsyncEnumerable<TOuter> outer,IAsyncEnumerable<TInner> inner,
+            Func<TOuter, Task<TKey>> outerKeySelector,
+            Func<TInner, Task<TKey>> innerKeySelector,
+            Func<TOuter, IAsyncEnumerable<TInner>, Task<TResult>> resultSelector,
+            IEqualityComparer<TKey> comparer)
+        {
+            if (outer == null) throw new ArgumentNullException("outer");
+            if (inner == null) throw new ArgumentNullException("inner");
+            if (outerKeySelector == null) throw new ArgumentNullException("outerKeySelector");
+            if (innerKeySelector == null) throw new ArgumentNullException("innerKeySelector");
+            if (resultSelector == null) throw new ArgumentNullException("resultSelector");
+            if (comparer == null) throw new ArgumentNullException("comparer");
+
+            var innerDictionary = new Dictionary<TKey, List<TInner>>(comparer);
+
+            return Create<TResult>(async producer =>
+            {
+                // Fill the inner set:
+                await inner.ForEach(async state =>
+                {
+                    var innerKey = await innerKeySelector(state.Item);
+                    if ( !innerDictionary.ContainsKey(innerKey))
+                    {
+                        List<TInner> innerList = null;
+                        if ( innerDictionary.TryGetValue(innerKey,out innerList))
+                        {
+                            innerList.Add(state.Item);
+                        }
+                        else
+                        {
+                            innerList = new List<TInner>() {state.Item};
+                            innerDictionary.Add(innerKey,innerList);
+                        }
+                    }
+                });
+
+                await outer.ForEach(async state =>
+                {
+                    var outerKey = await outerKeySelector(state.Item);
+                    List<TInner> innerList;
+                    if (innerDictionary.TryGetValue(outerKey, out innerList))
+                    {
+                        var result = await resultSelector(state.Item, innerList.ToAsync());
+                        await producer.Yield(result);
+                    }
+                });
+            });
+        }
+    }
+}
