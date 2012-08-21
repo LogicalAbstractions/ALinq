70c6bcdfe4f1e64de821f4bc3d66e601f9690544
BernhardGlueck <business@bernhardglueck.com>
2012-08-21 18:01:46 +0200

    Added the different intersection operators.

---
+++ Source/ALinq/AsyncEnumerable.Intersect.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ALinq
+{
+    public static partial class AsyncEnumerable
+    {
+        public static IAsyncEnumerable<T> Intersect<T>(this IAsyncEnumerable<T> first, IAsyncEnumerable<T> second)
+        {
+            return Intersect(first, second, EqualityComparer<T>.Default);
+        }
+
+        public static IAsyncEnumerable<T> Intersect<T>(this IAsyncEnumerable<T> first, IAsyncEnumerable<T> second, IEqualityComparer<T> comparer)
+        {
+            if (first == null) throw new ArgumentNullException("first");
+            if (second == null) throw new ArgumentNullException("second");
+            if (comparer == null) throw new ArgumentNullException("comparer");
+
+            return Create<T>(async producer =>
+            {
+                var set = new HashSet<T>();
+#pragma warning disable 1998
+                await second.ForEach(async item => set.Add(item));
+#pragma warning restore 1998
+
+                await first.ForEach(async item =>
+                {
+                    if (set.Contains(item))
+                    {
+                        await producer.Yield(item);
+                    }
+                });
+            });
+        }
+    }
+}
---
+++ Source/ALinq/AsyncEnumerable.Union.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ALinq
+{
+    public static partial class AsyncEnumerable
+    {
+        public static IAsyncEnumerable<T> Union<T>(this IAsyncEnumerable<T> first, IAsyncEnumerable<T> second)
+        {
+            return Union(first, second, EqualityComparer<T>.Default);
+        }
+
+        public static IAsyncEnumerable<T> Union<T>(this IAsyncEnumerable<T> first, IAsyncEnumerable<T> second, IEqualityComparer<T> comparer)
+        {
+            if (first == null) throw new ArgumentNullException("first");
+            if (second == null) throw new ArgumentNullException("second");
+            if (comparer == null) throw new ArgumentNullException("comparer");
+
+            return Create<T>(async producer =>
+            {
+                var set = new HashSet<T>();
+
+                await first.ForEach(async item =>
+                {
+                    if (!set.Contains(item))
+                    {
+                        set.Add(item);
+                        await producer.Yield(item);
+                    }
+                });
+
+                await first.ForEach(async item =>
+                {
+                    if (!set.Contains(item))
+                    {
+                        set.Add(item);
+                        await producer.Yield(item);
+                    }
+                });
+            });
+        }
+    }
+}
